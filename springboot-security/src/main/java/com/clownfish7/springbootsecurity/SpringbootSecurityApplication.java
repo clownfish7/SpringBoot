28510190f6bae48861c14463b4df6d4367a74041
clownfish <279505647@qq.com>
2019-07-14 22:16:58 +0800

    security

---
+++ springboot-security/src/main/java/com/clownfish7/springbootsecurity/SpringbootSecurityApplication.java
@@ -0,0 +1,19 @@
+package com.clownfish7.springbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 1、引入SpringSecurity；
+ * 2、编写SpringSecurity的配置类；
+ *      @EnableWebSecurity extends WebSecurityConfigurerAdapter
+ * 3、控制请求的访问权限；
+ */
+@SpringBootApplication
+public class SpringbootSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityApplication.class, args);
+    }
+
+}
